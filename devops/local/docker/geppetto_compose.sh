540ec214e05650bae905e909d83a130109ed11a6
aharishsundhar <harish.s@10decoders.in>
2021-04-23 18:28:14 +0000

    code commited from geppetto!

---
+++ devops/local/docker/geppetto_compose.sh
@@ -0,0 +1,73 @@
+#!bin/bash
+
+DESKTOPCODE='../../../application/client/desktop/sample26'
+
+COMPOSEPATH='../../../../devops/local/docker/'
+
+ENVPATH='../../.env'
+
+
+while getopts :cdrs option
+do
+    case "$option" in
+    c)  
+         echo "Creating new docker images and containers"
+         cd $DESKTOPCODE
+         docker build -t sample26ui-5080 .
+         docker run --name sample26ui-5080 --restart=unless-stopped -d -p 5055:5000 sample26ui-5080
+         sleep 15
+         echo "UI build is done..."
+
+         cd $COMPOSEPATH
+         docker-compose up -d --build
+         echo "uploading the mongo script..."
+         sleep 50
+         docker cp mongo.js mongo-5080:/data/db/
+         docker exec -ti mongo-5080 mongo -u admin -p 'password' --authenticationDatabase 'admin' sample26_5080 /data/db/mongo.js
+         sleep 10
+         echo "Process completed"
+         echo " Your application is deployed here the link, http://localhost:5055 "
+         ;;
+    d)
+         echo "Now Deleting all containers and images"
+         docker-compose down -v --rmi all 
+         docker rm -f sample26ui-5080
+         docker rmi sample26ui-5080
+         echo "Process completed"
+         ;;
+    r)
+         echo "Now Re-starting the stopped containers"
+         docker-compose start
+         docker restart sample26ui-5080
+         sleep 35
+         echo "Process completed"
+         ;;
+    s)
+         echo "Now stopping the running containers"
+         docker-compose stop
+         docker stop sample26ui-5080
+         echo "Process completed"
+         ;;
+    *)
+        echo "Hmm, an invalid option was received. the valid option are."
+        echo "Flag c - To Create new containers and images."
+        echo "Flag d - To Delete all the containers and images."
+        echo "Flag r - To Restart the stopped containers."
+        echo "Flag s - To Stop the running containers."
+        echo "Here's the usage statement:"
+        echo ""
+        echo "bash geppetto_compose.sh -c (or) bash geppetto_compose.sh -d (or) bash geppetto_compose.sh -r (or) bash geppetto_compose.sh -s"
+       
+       ;;
+        esac
+done
+
+echo ""
+echo "These are the usage options for help."
+echo "Flag c - To Create new containers and images."
+echo "Flag d - To Delete all the containers and images."
+echo "Flag r - To Restart the stopped containers."
+echo "Flag s - To Stop the running containers."
+echo "Here's the usage statement:"
+echo ""
+echo "bash geppetto_compose.sh -c (or) bash geppetto_compose.sh -d (or) bash geppetto_compose.sh -r (or) bash geppetto_compose.sh -s"
